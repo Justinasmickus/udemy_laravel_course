4b45de9ebeb80d7102dd73c431bfef56980201bf
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-03 18:06:25 +0200

    reusable global scopes

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -10,7 +10,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Contact extends Model
 {
     use HasFactory;
+    
     protected $fillable = ['first_name','last_name','email','phone','address','company_id'];
+
+    public $filterColumns = ['company_id'];
 
     public function company()
     {
